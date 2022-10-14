69da92c0a2ee55086324335967c848bb6e7affd1
Matheus Feitoza <matheus.feitoza@neosteel.com.br>
2022-10-14 09:21:51 -0300

    CI: added CI

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+    viteFinal: (config, {configType}) => {
+      if(confitType === 'PRODUCTIONS') {
+          config.base = '/ignite-lab-design-system'
+      }
+        return config;
   }
 }
