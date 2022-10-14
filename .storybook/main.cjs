8013247061209ca187dd2aac3db434b8ab9ddbde
Matheus Feitoza <matheus.feitoza@neosteel.com.br>
2022-10-14 09:24:56 -0300

    CI: Fix CI configs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,7 +16,7 @@ module.exports = {
     "storyStoreV7": true
   },
     viteFinal: (config, {configType}) => {
-      if(confitType === 'PRODUCTIONS') {
+      if(configType === 'PRODUCTIONS') {
           config.base = '/ignite-lab-design-system'
       }
         return config;
